f0db298f2655e1e2c1907ba1631ebbf06a86a96a
Vaseem Mohammed <vaseem.mohammed@ugamsolutions.com>
2015-07-20 17:49:00 +0530

    conflict removed and pushed with existing new api url

--- decorpot-web/src/main/webapp/resources/core/js/application/controllers.js
+++ decorpot-web/src/main/webapp/resources/core/js/application/controllers.js
@@ -1,34 +1,17 @@
 /**
  * @author: vaseem.mohammed
- * @date  : 12-07-2015
+ * @date : 12-07-2015
  */
 var decorpotCtrls = angular.module('decorpot');
 
-decorpotCtrls.controller('DecorpotCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
-	
-}]);
+decorpotCtrls.controller('DecorpotCtrl', [ '$scope', '$routeParams', function($scope, $routeParams) {
 
-<<<<<<< HEAD
-decorpotCtrls.controller('ImageListController', ['$scope', '$routeParams', 'interiors', function($scope, $routeParams, interiors){
-=======
-decorpotCtrls.controller('ImageListController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http, $q){
->>>>>>> branch 'master' of https://github.com/shomil/decorpot.git
-	
-	$scope.imageList = {};
-<<<<<<< HEAD
-	/*var responsePromise = $http.get("/test");
-=======
-	var responsePromise = $http.get("imageListSpace/living?from=300000&to=200000&page=1");
->>>>>>> branch 'master' of https://github.com/shomil/decorpot.git
-	responsePromise.success(function(data, status, headers, config) {
-        $scope.imageList = data;
-        console.log(data);
-    });
-    responsePromise.error(function(data, status, headers, config) {
-        alert("AJAX failed!");
-    });*/
-    
-    interiors.getImageList(function(data, status, headers, config){
-    	$scope.imageList = data;
-    });
-}]);
+		} ]);
+
+decorpotCtrls.controller('ImageListController', [ '$scope', '$routeParams', 'interiors', function($scope, $routeParams, interiors) {
+			$scope.imageList = {};
+
+			interiors.getImageList(function(data, status, headers, config) {
+				$scope.imageList = data;
+			});
+		} ]);
--- decorpot-web/src/main/webapp/resources/core/js/application/services.js
+++ decorpot-web/src/main/webapp/resources/core/js/application/services.js
@@ -2,7 +2,7 @@ var services = angular.module('decorpot');
 
 services.service('interiors', function($http){
 	this.getImages = function(callback){
-		var testPromise = $http.get("/test");
+		var testPromise = $http.get("imageListSpace/living?from=300000&to=200000&page=1");
 		testPromise.success(callback);
 		testPromise.error(function(data, status, headers, config) {
 	        alert("AJAX failed!");

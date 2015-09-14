1b9541a17edad278c4368de346fda384baa27bc0
sameer2904 <sameersaurav2904@gmail.com>
2015-09-15 00:03:03 +0530

    image path changes

--- decorpot-services/src/main/java/com/decorpot/services/PastProjectServiceImpl.java
+++ decorpot-services/src/main/java/com/decorpot/services/PastProjectServiceImpl.java
@@ -14,6 +14,7 @@ public class PastProjectServiceImpl implements PastProjectService {
 	@Autowired
 	private PastProjectsRepo pastProjectRepo;
 	private final String smallImageUrl =  "https://s3-ap-southeast-1.amazonaws.com/decorpot/work_done_low/";
+	private final String hdImageUrl =  "https://s3-ap-southeast-1.amazonaws.com/decorpot/work_done_hd/43.jpg";
 	
 	@Override
 	public List<String> getAllProjects() {
@@ -26,7 +27,7 @@ public class PastProjectServiceImpl implements PastProjectService {
 		List<Map<String, Object>> images = pastProjectRepo.getImagesByAppartment(project.replaceAll("-", " "));
 		for(Map<String, Object> image : images){
 			image.put("SMALL_PATH", smallImageUrl + image.get("SMALL_PATH").toString());
-			image.put("HD_PATH", smallImageUrl + image.get("HD_PATH").toString());
+			image.put("HD_PATH", hdImageUrl + image.get("HD_PATH").toString());
 		}
 		return images;
 	}

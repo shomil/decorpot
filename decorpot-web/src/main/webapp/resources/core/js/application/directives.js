fb056c00f00d24379c39c5ab0bceacad7afeab85
sameer2904 <sameersaurav2904@gmail.com>
2015-10-01 14:23:50 +0530

    directive for dynamic html

--- decorpot-web/src/main/webapp/resources/core/js/application/directives.js
+++ decorpot-web/src/main/webapp/resources/core/js/application/directives.js
@@ -336,3 +336,17 @@ decortDirectives
 						restrict : "A"
 					});
 				});
+
+decortDirectives
+.directive('dynamic', function($compile){
+	return{
+		restrict: 'A',
+		replace: true,
+		link : function(scope, ele, attrs){
+			scope.$watch(attrs.dynamic, function(html){
+				ele.html(html);
+				$compile(ele.contents())(scope);
+			});
+		}
+	};
+});
